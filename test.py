029fdbc05e146e99e98b5ce3f5a4d6079ff8dbcb
Shahzaib <shahzaib.khan@xloopdigital.com>
2023-01-18 17:10:09 +0500

    test file updated

--- test.py
+++ test.py
@@ -1,3 +1,8 @@
+import numpy as np
 import pandas as pd
 
-print("Hello World!")
+print(np.__version__)
+print(pd.__version__)
+print(
+    "Helloooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooooo World!"
+)
