4e3bebb956eaf7e99b07b488df4b279bfb242d8c
S-Saifali <syed.saif.ali210@gmail.com>
2023-02-06 20:06:25 +0500

    Adding kafka server and pushing data of both sensors to their respective topics (carbonsense and moisturemate)

--- extract/carbon_moist_api/carbon_moist_data_extract_app.py
+++ extract/carbon_moist_api/carbon_moist_data_extract_app.py
@@ -1,25 +1,33 @@
 import logging
-
+from kafka import KafkaProducer
 import uvicorn
 from fastapi import FastAPI, Request
-
-app = FastAPI()
+import json
+from typing import Dict
+import os
 
 logger = logging.getLogger()
+app = FastAPI()
 
+KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
+Producer = KafkaProducer(
+            bootstrap_servers=KAFKA_BROKER_URL,
+            value_serializer=lambda x: json.dumps(x).encode('utf8'),
+            api_version=(0, 10, 1)
+        )
 
 @app.post("/api/fetch/moisturemate_data")
 async def collect_moisture_mate_data(request: Request):
-    moisture_mate_received_data = await request.json()
-    logger.info(f"Received MoistureMate data: {moisture_mate_received_data}")
-    return {"msg": "received moisture_mate data"}
-
+    moisturemate_data = await request.json()
+    logger.info(f"Received MoistureMate Data: {moisturemate_data}")
+    record = Producer.send('moisturemate', value=moisturemate_data)
+   
 
 @app.post("/api/fetch/carbonsense_data")
 async def collect_carbonsense_data(request: Request):
-    carbonsense_received_data = await request.json()
-    logger.info(f"Received CarbonSense data: {carbonsense_received_data}")
-    return {"msg": "received carbonsense data"}
+    carbonsense_data = await request.json()
+    logger.info(f"Received Carbonsense Data: {carbonsense_data}")
+    record=Producer.send('carbonsense', value=carbonsense_data)
 
 
 def run_app():
