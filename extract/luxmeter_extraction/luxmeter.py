7d4d92c7742238155dfad4d1562c4a2bb73929ab
S-Saifali <syed.saif.ali210@gmail.com>
2023-02-07 10:48:27 +0500

    Ran pre-commit on this branch

--- extract/luxmeter_extraction/luxmeter.py
+++ extract/luxmeter_extraction/luxmeter.py
@@ -1,31 +1,33 @@
-import logging
-import requests
-import time
-import os
-from kafka import KafkaProducer
 import json
+import logging
+import os
+import time
+
+import requests
+from kafka import KafkaProducer
 
 logger = logging.getLogger()
 KAFKA_BROKER_URL = os.environ.get("KAFKA_BOOTSTRAP_SERVER")
 Producer = KafkaProducer(
-            bootstrap_servers=KAFKA_BROKER_URL,
-            value_serializer=lambda x: json.dumps(x).encode('utf8'),
-            api_version=(0, 10, 1)
-        )
+    bootstrap_servers=KAFKA_BROKER_URL,
+    value_serializer=lambda x: json.dumps(x).encode('utf8'),
+    api_version=(0, 10, 1)
+)
 
 rooms = ["kitchen", "bedroom", "bathroom", "living_room"]
 
 
 def luxmeter_data():
-    while(True):
-        time.sleep(60) 
+    while (True):
+        time.sleep(60)
         for room_id in rooms:
-            received_data = requests.get(f'http://sensor:3000/api/luxmeter/{room_id}')
-            received_data = received_data.json()  
+            received_data = requests.get(
+                f'http://sensor:3000/api/luxmeter/{room_id}')
+            received_data = received_data.json()
             last_record = f"{received_data['room_id']}: {received_data['measurements'][-1]}"
-            record=Producer.send('luxmeter', value=last_record)
+            record = Producer.send('luxmeter', value=last_record)
             logger.info(f"Received Luxmeter Data: {last_record}")
-            
+
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
