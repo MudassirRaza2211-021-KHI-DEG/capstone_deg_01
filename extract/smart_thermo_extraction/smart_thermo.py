2effd80c619830056d73c931fb3f841490e850f5
Syed Saif Ali <syed.saif.ali210@gmail.com>
2023-02-11 00:19:49 +0500

    Incorporated modularity in smart_thermo_function

--- extract/smart_thermo_extraction/smart_thermo.py
+++ extract/smart_thermo_extraction/smart_thermo.py
@@ -18,28 +18,33 @@ producer = KafkaProducer(
         )
 
 
-def smartthermo_data():
+def get_smartthermo_data_periodically():
+    while(True):
+        time.sleep(60)
+        get_smart_thermo_data()
+
+
+def get_smart_thermo_data():
     # Connect to Minio using the S3 protocol
     s3 = boto3.resource("s3",
                         endpoint_url="http://minio:9000",
                         aws_access_key_id=minio_access_key,
                         aws_secret_access_key=minio_secret_key)
-    while(True):
-        time.sleep(60)
-        # List all objects in the bucket
-        bucket = s3.Bucket("capstondeg01")
-        # filter method will return a list of all those csv files in the folder.
-        objects = list(bucket.objects.filter())
-        # Get last object
-        obj = objects[-1]
-        # Read the contents of the object if it's a csv file
-        if obj.key.endswith(".csv"):
-            obj = s3.Object("capstondeg01", obj.key)
-            content = obj.get()["Body"].read().decode("utf-8")
-            logger.info(f"Read SmartThermo data from MinIO: {content}")
-            record=producer.send('smartthermo', value=content)
+     
+    # List all objects in the bucket
+    bucket = s3.Bucket("capstondeg01")
+    # filter method will return a list of all those csv files in the folder.
+    objects = list(bucket.objects.filter())
+    # Get last object
+    obj = objects[-1]
+    # Read the contents of the object if it's a csv file
+    if obj.key.endswith(".csv"):
+        obj = s3.Object("capstondeg01", obj.key)
+        content = obj.get()["Body"].read().decode("utf-8")
+        logger.info(f"Read SmartThermo data from MinIO: {content}")
+        record=producer.send('smartthermo', value=content)
 
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
-    smartthermo_data()
+    get_smartthermo_data_periodically()
