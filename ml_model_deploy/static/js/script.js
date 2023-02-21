e4550ecd4e713a1f460414512bd7ec475a7ecb2e
shahzaib.khan@xloopdigital.com <shahzaib.khan@xloopdigital.com>
2023-02-22 00:26:58 +0500

    removed commented part as mentioned

--- ml_model_deploy/static/js/script.js
+++ ml_model_deploy/static/js/script.js
@@ -10,18 +10,3 @@ $(document).ready(function(){
         autoPlay:true
     });
 });
-
-
-
-$( document ).ready(function() {
-    var w = window.innerWidth;
-
-    if(w > 767){
-        $('#menu-jk').scrollToFixed();
-    }else{
-       // $('#menu-jk').scrollToFixed();
-    }
-
-
-
-})
