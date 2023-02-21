c0be106ab178656d6daf6e4331373feae1cd09b1
S-Saifali <syed.saif.ali210@gmail.com>
2023-02-21 12:52:44 +0500

    Added static folder which contains our frontend assets

---
+++ ml_model_deploy/static/js/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+$(document).ready(function(){
+    $("#testimonial-slider").owlCarousel({
+        items:2,
+        itemsDesktop:[1000,2],
+        itemsDesktopSmall:[979,2],
+        itemsTablet:[768,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
+
+
+
+$( document ).ready(function() {
+    var w = window.innerWidth;
+
+    if(w > 767){
+        $('#menu-jk').scrollToFixed();
+    }else{
+       // $('#menu-jk').scrollToFixed();
+    }
+
+
+
+})
