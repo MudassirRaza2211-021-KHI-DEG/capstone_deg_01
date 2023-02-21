83f3ab01791bf95034df97fed055ce565d5c551e
shahzaib.khan@xloopdigital.com <shahzaib.khan@xloopdigital.com>
2023-02-21 12:40:27 +0500

    sql file added to create our table

---
+++ sql/create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS mytable (
+  timestamp VARCHAR(50),
+  room_id VARCHAR(50),
+  humidity NUMERIC(10, 6),
+  humidity_ratio NUMERIC(10, 6),
+  co2 NUMERIC(10, 6),
+  light_level NUMERIC(10, 6),
+  temperature NUMERIC(10, 6)
+);
